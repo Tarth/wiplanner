4c68538d16535cb6d5b51f5c0c3162909e9586e9
MBE <zirax83@gmail.com>
2020-06-17 10:29:56 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function Container() {
+  const [items, setItems] = useState(null);
+
+  if (items === null) {
+    DataHandler(setItems);
+  }
+
+  return <div>Stuff goes here</div>;
+}
+
+function DataHandler(setItems) {
+  let data = require("./sampledata.json");
+  let jobdata = [];
+
+  for (let i = 0; i < data.jobs.length; i++) {
+    let username = data.jobs[i].user;
+    let startdate = data.jobs[i].startdate;
+    let startmonth = data.jobs[i].startmonth;
+    let startyear = data.jobs[i].startyear;
+    let enddate = data.jobs[i].enddate;
+    let endmonth = data.jobs[i].endmonth;
+    let endyear = data.jobs[i].endyear;
+
+    let startdato = new Date(startyear, startmonth, startdate);
+    let slutdato = new Date(endyear, endmonth, enddate);
+
+    let job = { username: username, start: startdato, end: slutdato };
+    jobdata.push(job);
+  }
+  setItems(jobdata);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div className="room">
+    <Container />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
